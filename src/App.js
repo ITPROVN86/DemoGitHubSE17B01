6f3d8d528da711242f3728552238b67bae8d0af6
ITPROVN86 <quangltn3@fe.edu.vn>
2023-09-09 15:54:55 +0700

    first code

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import * as React from "react";
+
+const initialState = {
+  options: [
+    { id: 1, name: "First", value: 10 },
+    { id: 2, name: "Second", value: 50 },
+    { id: 3, name: "Third", value: 200 },
+  ],
+  quantity: 1,
+  selected: 1,
+};
+
+function reduceButtonStates(state) {
+  return {
+    ...state,
+    decrementDisabled: state.quantity === 0,
+    incrementDisabled: state.quantity === 10,
+  };
+}
+
+function reduceTotal(state) {
+  const option = state.options.find(
+    (option) => option.id === state.selected
+  );
+  return { ...state, total: state.quantity * option.value };
+}
+
+function reducer(state, action) {
+  let newState;
+  switch (action.type) {
+    case "init":
+      newState = reduceTotal(state);
+      return reduceButtonStates(newState);
+    case "decrementQuantity":
+      newState = { ...state, quantity: state.quantity - 1 };
+      newState = reduceTotal(newState);
+      return reduceButtonStates(newState);
+    case "incrementQuantity":
+      newState = { ...state, quantity: state.quantity + 1 };
+      newState = reduceTotal(newState);
+      return reduceButtonStates(newState);
+    case "selectItem":
+      newState = { ...state, selected: Number(action.id) };
+      return reduceTotal(newState);
+    default:
+      throw new Error(`${action.type} is not a valid action`);
+  }
+}
+
+export default function App() {
+  const [
+    {
+      options,
+      selected,
+      quantity,
+      total,
+      decrementDisabled,
+      incrementDisabled,
+    },
+    dispatch,
+  ] = React.useReducer(reducer, initialState);
+
+  React.useEffect(() => {
+    dispatch({ type: "init" });
+  }, []);
+
+  return (
+    <>
+      <section>
+        <button
+          disabled={decrementDisabled}
+          onClick={() => dispatch({ type: "decrementQuantity" })}
+        >
+          -
+        </button>
+        <button
+          disabled={incrementDisabled}
+          onClick={() => dispatch({ type: "incrementQuantity" })}
+        >
+          +
+        </button>
+        <input readOnly value={quantity} />
+      </section>
+      <section>
+        <select
+          value={selected}
+          onChange={(e) =>
+            dispatch({ type: "selectItem", id: e.target.value })
+          }
+        >
+          {options.map((o) => (
+            <option key={o.id} value={o.id}>
+              {o.name}
+            </option>
+          ))}
+        </select>
+      </section>
+      <section>
+        <strong>{total}</strong>
+      </section>
+    </>
+  );
+}
+
+
+//  import logo from './logo.svg';
+// import './App.css';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           FPT University
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
